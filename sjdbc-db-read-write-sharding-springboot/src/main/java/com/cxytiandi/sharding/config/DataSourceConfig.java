6336ef95dfeed0ade2efbe20df05738d01294dd5
yangsumeng <584903541@qq.com>
2020-06-21 23:50:40 +0800

    init

---
+++ sjdbc-db-read-write-sharding-springboot/src/main/java/com/cxytiandi/sharding/config/DataSourceConfig.java
@@ -0,0 +1,18 @@
+package com.cxytiandi.sharding.config;
+
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class DataSourceConfig {
+
+//    /**
+//     * 配置数据源0，数据源的名称最好要有一定的规则，方便配置分库的计算规则
+//     *
+//     * @return
+//     */
+//    @Bean(name = "dataSource0")
+//    @ConfigurationProperties(prefix = "spring.datasource.test1")
+//    public DataSource dataSource0() {
+//        return DataSourceBuilder.create().build();
+//    }
+}
